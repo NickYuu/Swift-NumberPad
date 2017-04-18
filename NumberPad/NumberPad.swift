523c68cadfb3aba6e3545626e61c1b15d217803f
NickYuu <s3310304067@gms.nutc.edu.tw>
2017-04-18 17:35:32 +0800

    first commit

---
+++ NumberPad/NumberPad.swift
@@ -0,0 +1,21 @@
+//
+//  NumberPad.swift
+//  NumberPad
+//
+//  Created by TsungHan on 2017/4/18.
+//  Copyright © 2017年 TsungHan. All rights reserved.
+//
+
+import UIKit
+
+class NumberPad: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
